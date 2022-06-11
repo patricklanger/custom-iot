1c2fef1455d5ba1754af02174a450b087a87d8b7
patrick-recyclehero <95284442+patrick-recyclehero@users.noreply.github.com>
2022-06-11 10:31:23 +0200

    aufgabe 2a - autoregistrierung

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 /*
  * Aktueller Stand:
  * 1. coap_saul funktioniert
- * 2. cord_ep muss implementiert werden
+ * 2. cord_ep ist implementiert werden. muss getestet werden (Aufgabe 2.a)
+ * 3. Aufgabe 2.b:
+ *     Wie bekommen wir die Adresse des Pi automatisch?
+ *     Jedem IoT Knoten im lowpan Netz des Pi wird die Routeradresse (ABRO) mitgeteilt.
+ *     irgendwie müssten wir auf diese zugreifen können.
+ *     oder IP als environment variable übergeben?
+ * 4. Aufgabe 2.c:
+ *     python... client: irgendwas damit?? https://aiocoap.readthedocs.io/en/latest/examples.html
  */
 
 #include <stdio.h>
@@ -13,6 +20,9 @@
 #include "gcoap_example.h"
 #include "gcoap_saul.h"
 
+#include "net/cord/common.h"
+#include "net/cord/ep_standalone.h"
+
 #define MAIN_QUEUE_SIZE (4)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
@@ -22,21 +32,47 @@ static const shell_command_t shell_commands[] = {
 };
 
 // TODO einbauen wird für cord_ep_standalone_reg_cb benötigt
-// /* we will use a custom event handler for dumping cord_ep events */
-// static void _on_ep_event(cord_ep_standalone_event_t event)
-// {
-//     switch (event) {
-//         case CORD_EP_REGISTERED:
-//             puts("RD endpoint event: now registered with a RD");
-//             break;
-//         case CORD_EP_DEREGISTERED:
-//             puts("RD endpoint event: dropped client registration");
-//             break;
-//         case CORD_EP_UPDATED:
-//             puts("RD endpoint event: successfully updated client registration");
-//             break;
-//     }
-// }
+/* we will use a custom event handler for dumping cord_ep events */
+static void _on_ep_event(cord_ep_standalone_event_t event)
+{
+    switch (event) {
+        case CORD_EP_REGISTERED:
+            puts("RD endpoint event: now registered with a RD");
+            break;
+        case CORD_EP_DEREGISTERED:
+            puts("RD endpoint event: dropped client registration");
+            break;
+        case CORD_EP_UPDATED:
+            puts("RD endpoint event: successfully updated client registration");
+            break;
+    }
+}
+
+static void register_on_rd(char *ip)
+{
+    // remote endpoint of the target Resource Directory (RD)
+    sock_udp_ep_t remote;
+
+    // Im _cord_ep_handler (Shell command) ist
+    // argv[0] = cord_ep
+    // argv[1] = register
+    // argv[2] = ip der Service Discovery (Pi) ist jetzt char *ip
+    // argv[3] = interface
+    if (make_sock_ep(&remote, ip) < 0) {
+        printf("error: unable to parse address\n");
+        return 1;
+    }
+    puts("Registering with RD now, this may take a short while...");
+    // NULL weil wir kein registration interface angeben wollen sondern es
+    // automatisch bestimmt wird.
+    if (cord_ep_register(&remote, NULL) != CORD_EP_OK) {
+        puts("error: registration failed");
+    }
+    else {
+        puts("registration successful\n");
+        cord_ep_dump_status();
+    }
+}
 
 int main(void)
 {
@@ -48,9 +84,12 @@ int main(void)
     gcoap_saul_init();
     puts("gcoap_saul_init app");
 
-//TODO cord_ep ausführen
-    // /* register event callback with cord_ep_standalone */
-    // cord_ep_standalone_reg_cb(_on_ep_event);
+    // TODO cord_ep ausführen
+    /* register event callback with cord_ep_standalone */
+    cord_ep_standalone_reg_cb(_on_ep_event);
+
+    char* ip = [2001:67c:254:b0b2:affe:2000:0:1];
+    register_on_rd(ip);
 
 //TODO bei aiocoap-rd anmelden also nicht mehr über shell befehl sondern hierüber
 // RIOT-Shellbefehle findet man unter RIOT/sys/shell/commands
