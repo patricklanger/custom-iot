9051db1b8acdf87d81f83a5ab28467289f4abaa0
patrick-recyclehero <95284442+patrick-recyclehero@users.noreply.github.com>
2022-06-29 18:38:28 +0200

    trying wot consumer

--- gcoap_saul.c
+++ gcoap_saul.c
@@ -58,8 +58,12 @@ static ssize_t saul_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, void *ctx
 
             /* write value to payload (json format) */
             //Speichern ergebnis in resp_len / wir wandeln fuer export in json um
-            resp_len += snprintf((char*)pdu->payload, len, "{\"value\": 25}");
-            // resp_len += phydat_to_json(&data, dim, (char*)pdu->payload);
+            char valueBuf[20];
+            phydat_to_json(&data, dim, valueBuf);
+
+            snprintf((char*)pdu->payload, len, "{\"value\": %s}", valueBuf);
+            printf("%s\n", (char*)pdu->payload);
+
             return resp_len;
 
         case COAP_PUT:
