45cbe6733e12b2aa615ded94c8237b481c9ece57
patrick-recyclehero <95284442+patrick-recyclehero@users.noreply.github.com>
2022-06-29 17:08:37 +0200

    trying wot consumer

--- Frontend/wot-consumer.py
+++ Frontend/wot-consumer.py
@@ -28,10 +28,8 @@ DESCRIPTION = {
     "title": "MyLampThing",
     "properties": {
         "status" : {
-            "name": "statusName",
             "type": "string",
             "forms": [{
-                "name": "URLName",
                 "op": "readproperty",
                 "contentType": "text/plain",
                 "href": "coap://[2001:67c:254:b0b2:affe:2896:134b:16e6]/saul/9-mpl3115a2-SENSE_TEMP"}]
