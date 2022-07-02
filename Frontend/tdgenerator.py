60da4fe0919c11ad06478e759e10ce921efe8a54
Isabell Egloff <isabellegloff@Isabells-iMac.fritz.box>
2022-07-02 14:56:50 +0200

    Final for TI

--- Frontend/tdgenerator.py
+++ Frontend/tdgenerator.py
@@ -14,15 +14,14 @@ logging.basicConfig()
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 
-# Delete me -----------------------
-ID_THING = "urn:temperaturething"
+#  --------------------------------
 temp_url = "coap://[2001:67c:254:b0b2:affe:2896:134b:16e6]/saul/9-mpl3115a2-SENSE_TEMP"
 # ---------------------------------
 
 RD_URL = "coap://localhost/resource-lookup/"
 
 DESCRIPTION = {
-    "id": "urn:device",
+    "id": "",
     "@context": "https://www.w3.org/2022/wot/td/v1.1",
     "title": "IoT Device",
     "description": "IoT Sensor Device that sense humidity, temperature and light color",
@@ -70,7 +69,7 @@ async def get_resources(ctx):
     request = Message(code=Code.GET, uri=RD_URL)
 
     try:
-        # Alle resourcen alleer diveces abfragen
+        # Alle resourcen aller diveces abfragen
         response = await ctx.request(request).response
     except Exception as e:
         print('Failed to fetch resource:')
@@ -90,40 +89,46 @@ async def device_registration(context):
     Links zu entsprechenden resourcen werden den td-dicts hinzugefügt
     """
     print("device_registration")
-    devices = []
+    # Das wird die Liste fuer die Thing Descriptions
+    td_list = []
+    # get_resources gibt uns die URLs zu allen Ressourcen, die wir haben zurueck
     resources = await get_resources(context)
     # für jede IP die in den Resourcen vorkommt lege eine TD an
     # ... mehrere IPs kommen vor wenn es mehrere Things gibt.
     for url in resources:
         ip = url.split("/")[2]
-        if [td for td in devices if td['id'] == ip]:
+        if [td for td in td_list if td['id'] == ip]:
+            # Fuer jede neue IP wird neue DESCRIPTION erstellt (TD pro Device)
             td = copy(DESCRIPTION)
+            # die Id bekommt die IP
             td['id'] = ip
-            # TODO td['base'] = "coap://[2001:67c:254:b0b2:affe:2896:134b:16e6]/saul/9-mpl3115a2-SENSE_TEMP".split("/saul")[0]
-            devices.append(td)
+            # Thing Description fuegen wir in die Liste ein
+            td_list.append(td)
     # füge den angelegten TDs die urls zu ihren resourcen zu.
     for url in resources:
-        td = [td for td in devices if td['id'] == url.split("/")[2]][0]
+        td = [td for td in td_list if td['id'] == url.split("/")[2]][0]
         if 'SENSE_TEMP' in url:
             td["properties"]["temperature"]["forms"]["href"] = url
         if 'SENSE_HUM' in url:
             td["properties"]["humidity"]["forms"]["href"] = url
         if 'SENSE_COLOR' in url:
             td["properties"]["color"]["forms"]["href"] = url
-    return devices
+    return td_list
 
 
 class TDGenerator:
     """
-    Erstellt für alle devices in der RD eine TD als dict.
+    Erstellt für alle devices in der Resource Directory eine TD als dictionary.
     """
     def __init__(self):
         self.td_list = []
 
+    # Context wird erzeugt
     async def startup(self):
         context = await Context.create_client_context()
         await asyncio.sleep(3)
 
+        # Erzeugen einer Liste von Thing Descriptions
         self.td_list = await device_registration(context)
         print(f"registration for {len(self.td_list)} finished")
 
